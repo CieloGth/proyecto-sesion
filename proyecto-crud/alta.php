fa36cc2508324dd926cadd2b3f90a4be2932ccfc
CieloGth <102554792+CieloGth@users.noreply.github.com>
2022-07-29 15:08:31 -0500

    alta

---
+++ proyecto-crud/alta.php
@@ -0,0 +1,78 @@
+
+<?php
+    // function recursividad($a)
+    // {
+    //     if ($a < 20) {
+    //         echo "$a\n";
+    //         recursividad($a + 1);
+    //     }
+    // }
+    if(count($_POST)>0) {
+        function verificacion(){
+            // $is_int = filter_var($_POST["telefono"], FILTER_VALIDATE_INT);
+            // var_dump($is_int);
+            // $is_email = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
+            // var_dump($is_email);
+            if($_POST["cont"]==$_POST["cont2"]){
+                echo"Igual";
+            }
+            else{
+                echo"equivocado";
+            }
+        }
+        
+    }
+    // $con = mysqli_connect('localhost','root',null,'test',3310) or die('Unable To connect');
+    // $insert = mysqli_query($con,"SELECT * FROM user WHERE usuario='" . $_POST["usuario"] . "' and password = '". md5($_POST["password"])."';");
+    // $insert = mysqli_query($con,"INSERT INTO usuarios ( `nombre`, `apellido`, `usuario`, `email`, `contraseña`, `nacimiento`, `telefono`) VALUES ('".$_POST[""]."','".$_POST[""]."','".$_POST[""]."','".$_POST[""]."','".$_POST[""]."','".$_POST[""]."','".$_POST[""]."');");
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alta</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <form name="form" method="post" action="" style="text-align: center;">
+            <div class="row">
+                <div class="col-6 p-5" style="text-align: justify;">
+                    <p>Nombre: </p>
+                    <input type="text" class="form-control form-control-lg mb-3" name="nombre">
+                    <br>
+                    <p>Apellido: </p>
+                    <input type="text" class="form-control form-control-lg mb-3" name="apellido">
+                    <br>
+                    <p>Fecha de nacimiento: </p>
+                    <input type="date" class="form-control form-control-lg mb-3" name="fecha">
+                    <br>
+                    <p>Telefono: </p>
+                    <input type="tel" class="form-control form-control-lg mb-3" name="telefono">
+                    <br>
+                </div>
+                <div class="col-6 p-5" style="text-align: justify;">
+                    <p>Usuario: </p>
+                    <input type="text" class="form-control form-control-lg mb-3" name="usuario">
+                    <br>
+                    <p>Email: </p>
+                    <input type="email" class="form-control form-control-lg mb-3" name="email">
+                    <br>
+                    <p>Contraseña: </p>
+                    <input type="text" class="form-control form-control-lg mb-3" name="cont">
+                    <br>
+                    <p>Confirmar contraseña: </p>
+                    <input type="text" class="form-control form-control-lg mb-3" name="cont2">
+                    <br>
+                </div>
+            </div>
+            <input type="submit" name="submit" value="Submit" onclick="">
+        </form>
+        <?php verificacion();?>
+    <script src="../js/bootstrap.min.js"></script>
+
+</body>
+</html>
